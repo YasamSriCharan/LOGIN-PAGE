5566681e06ffe3d7959aa466e3cdf784a29318fc
YasamSriCharan <2500030149@kluniversity.in>
2026-02-17 10:28:02 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/STYLING-AND-COMPOSITION/',   // 👈 IMPORTANT
+  base: '/LOGIN-PAGE/',   // 👈 IMPORTANT
+})
